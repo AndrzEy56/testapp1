36e432c161e2fdc1b62f30e48a5bd994fb62085c
RomanInformatika <officepm@yandex.ru>
2023-02-09 19:13:02 +0300

    Кой чё сделал

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Роман Владимирович, засчет нашего ролика вас больше зауважали</Text>
+      <Text>Роман Владимирович, засчет нашего</Text>
       <StatusBar style="auto" />
     </View>
   );
